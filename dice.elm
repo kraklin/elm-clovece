9c4706f127a6c8e43887e7831e45c15cb8f201ad
Tomas Latal <latal.tomas@gmail.com>
2016-07-27 20:51:31 +0200

    Change dice to rotate automaticaly and stops

--- dice.elm
+++ dice.elm
@@ -1,11 +1,11 @@
-module Dice exposing (..)
+module Dice exposing (Model, Msg, init, update, view)
 
 import Html exposing (Html, button, div, text, img)
 import Html.App as Html
 import Html.Attributes exposing (src)
 import Html.Events exposing (onClick)
 import Random
-import Debug
+import Time exposing (Time, second)
 
 
 main : Program Never
@@ -23,13 +23,19 @@ main =
 
 
 type alias Model =
-    Int
+    { value : Int
+    , isRolling : Bool
+    , rollingTime : Time
+    }
 
 
 init : Model
 init =
-    (1)
-
+    { value = 1
+    , isRolling = False
+    , rollingTime = 10
+    }
+ 
 
 
 -- UPDATE
@@ -39,6 +45,7 @@ type Msg
     = Increment
     | Decrement
     | Roll
+    | GenerateNewFace Time
     | NewFace Int
 
 
@@ -46,22 +53,37 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            if model == 6 then
-                ( 6, Cmd.none )
+            if model.value == 6 then
+                ( { model | value = 6 }, Cmd.none )
             else
-                ( model + 1, Cmd.none )
+                ( { model | value = model.value + 1 }, Cmd.none )
 
         Decrement ->
-            if model == 1 then
-                ( 1, Cmd.none )
+            if model.value == 1 then
+                ( { model | value = 1 }, Cmd.none )
             else
-                ( model - 1, Cmd.none )
+                ( { model | value = model.value - 1 }, Cmd.none )
 
         Roll ->
-            ( model, Random.generate NewFace (Random.int 1 6) )
+            ( { model | isRolling = not model.isRolling }, Cmd.none )
+
+        GenerateNewFace time ->
+            if (model.rollingTime >= 600) then
+                ( { model
+                    | isRolling = False
+                    , rollingTime = 10
+                  }
+                , Cmd.none
+                )
+            else
+                ( { model
+                    | rollingTime = model.rollingTime * 2
+                  }
+                , Random.generate NewFace (Random.int 1 6)
+                )
 
         NewFace newFace ->
-            ( Debug.log "newFace:" newFace, Cmd.none )
+            ( { model | value = newFace }, Cmd.none )
 
 
 
@@ -72,7 +94,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , img [ src ("img/" ++ toString model ++ ".png"), onClick Roll ] []
+        , img [ src ("img/" ++ toString model.value ++ ".png"), onClick Roll ] []
         , button [ onClick Increment ] [ text "+" ]
         ]
 
@@ -83,4 +105,7 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    if model.isRolling then
+        Time.every model.rollingTime GenerateNewFace
+    else
+        Sub.none
