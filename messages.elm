703d66a3a0f5df245fcac9d06ce39a167910856e
Tomas Latal <latal.tomas@gmail.com>
2016-07-29 23:39:09 +0200

    added test file for imported dice

---
+++ messages.elm
@@ -0,0 +1,48 @@
+import Dice
+import Html.App as Html
+import TimeTravel.Html.App as TimeTravel
+import Html exposing (Html, div)
+
+-- import original counter from elm-architecture and try to bind the commands right
+main : Program Never
+main =
+    TimeTravel.program
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+type alias Model = {dice : Dice.Model}
+
+model: Model
+model = {
+        dice = Dice.init
+    }
+
+init : (Model, Cmd Msg)
+init = ({dice = Dice.init}, Cmd.none)
+
+type Msg =
+    ChangeDice Dice.Msg
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of 
+        ChangeDice msg ->
+            let (updateDiceMsg, diceCmd) = 
+                Dice.update msg model.dice
+            in
+            ({ model | dice = updateDiceMsg}, Cmd.map ChangeDice diceCmd )
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ 
+         Html.map ChangeDice (Dice.view model.dice)
+        ]
